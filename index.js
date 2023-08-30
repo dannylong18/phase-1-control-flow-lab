96bd170539fb68b6c06c249533deb2740179fd38
Daniel Long <danielwlong618@gmail.com>
2023-08-29 22:50:20 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(x){
+    if (x < 400) {
+      return "This one is on me!"
+    }
+    else if (x>400, x<2000) {
+      return "That will be twenty bucks."
+    }
+    else if (x>2000, x<2500) {
+      return "I will gladly take your thirty bucks."
+    }
+    else if (x>=2500) {
+      return "No can do."
+    }
 }
+scuberGreetingForFeet(199, 1500, 2001, 2501)
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+ return city === "NYC" ? 'Ok, sounds good.' : 'No go.';
 }
+ternaryCheckCity("NYC", "Pittsburgh")
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+   switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default :
+      return "Bye.";
+   }
 }
+switchOnCharmFromTip("generous", "not as generous", "thanks for everything")
